7037b42202ffb49223164d664c6dbd3ac5509fac
Master-Infrit <matheus.sam.pires@gmail.com>
2025-08-13 23:29:55 +0000

    fazendo a aula de struct

---
+++ pratica.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// ---  Constantes Globais  ---
+#define MAX_LIVROS 50
+#define TAM_STRING 100
+
+struct Livros {
+    char nome[TAM_STRING];
+    char autor[TAM_STRING];
+    char editora[TAM_STRING];
+    int edicao;
+};
+void LimparBufferEntrada() {
+    int c
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+int main(){
+    struct Livro biblioteca[MAX_LIVROS];
+    int totalLivros = 0;
+    int opcao;
+    
+    };
+    
+
+
+
+
+}
+
+
+};
